8c9472a7ccf41eeec666174a67454a4d113e098c
Brandonmkmk <brandonxd611@gmail.com>
2025-08-26 15:14:51 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './index.css' // Importante para Tailwind CSS
 function App() {
   return (
     <h1 className="text-4xl font-bold text-blue-500">
-      Hola Tailwind 🚀
+      Hola mundo 🚀
     </h1>
   )
 }
