aad2f35a1bdd57d69ebf38a389f8dcf212df5a0c
HectorAgHe <aguilarhector440@gmail.com>
2025-08-26 13:48:55 -0600

    Configuracion inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css' // Importante para Tailwind CSS
+
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">
+      Hola Tailwind 🚀
+    </h1>
+  )
+}
+
+export default App
---
+++ src/data/products.js
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 1,
+    title: "Carnitas",
+    description: "inspired by a Mexican Food.",
+    image: "/assets/products/carnitas.png",
+    types: ["Michoacan", "Ligth",],
+   
+  },
+  {
+    id: 2,
+    title: "Costilla",
+    description: "Meat from Pig.",
+    image: "/assets/products/costilla.png",
+    types: ["Para Azar", "Corte Cuadrado","Corte Carrillera"],
+    
+  }
+];
