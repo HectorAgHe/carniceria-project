7b624140ddea7ccbc1e6ca50e57a6cff996f5674
Brandon <104916330+ElVrabo@users.noreply.github.com>
2025-08-27 15:11:52 -0600

    add(navbar): adding navbar component (#3)

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,17 +2,21 @@
 
 import React from 'react'
 
+
 function ProductCard(props) {
     const {title,description,image,types} = props
   return (
-    <section>
-        <img src={image} alt='imagen del producto' />
-        <div>
-            <h1>{title}</h1>
+    <section className=' flex flex-col justify-center'  >
+        <img className='w-67 h-60' src={image} alt='imagen del producto' />
+        <div className='flex flex-col items-center mt-5' >
+            <h1 className='text-black' >{title}</h1>
             <p>{description}</p>
             {types.map((i)=>{
                 return <p key={i} >{i}</p>
             })}
+        </div>
+        <div className='mt-10 flex justify-center' >
+         <button className='bg-red-500 text-white w-64 border rounded-lg' >Detalles</button>
         </div>
     </section>
   )
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { products } from '../data/products'
 import ProductCard from '../components/ProductCard'
+import NavBar from '../components/NavBar'
 
 function Catalog() {
   return (
-    <section>
+    <main>
+        <NavBar/>
+        <section className='flex justify-center gap-10 mt-20' >
         {products.map((product)=>(
             <ProductCard
              key={product.id}
@@ -15,6 +18,7 @@ function Catalog() {
             />
         ))}
     </section>
+    </main>
   )
 }
 
